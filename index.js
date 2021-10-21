de76c0e79be4cc2f39bd74ac446b480fcefd284d
trottafede <ftrotta18@gmail.com>
2021-10-21 12:31:44 -0300

    asd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
