7f6e73bd0a204d0905cf06078bf835953cfa5c15
trottafede <ftrotta18@gmail.com>
2021-10-21 12:51:39 -0300

    set dir en ejs

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const blogs = require("./db");
 const app = express();
 const port = 3000;
+const path = require("path");
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
